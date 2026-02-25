ec8f018eaa12d3cd067fd15695f68361ba5e9dd5
othiagos <thg.santos@outlook.com>
2026-02-25 14:59:53 -0300

    Use the long format in the cli

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,22 +4,22 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 #[command(author, version, about = "TCOPS Solver Engine", long_about = None)]
 pub struct Cli {
-    #[arg(short, long, value_name = "FILE")]
+    #[arg(long, value_name = "FILE")]
     pub input: PathBuf,
 
-    #[arg(short, long, value_enum)]
+    #[arg(long, value_enum)]
     pub mode: SolverMode,
 
     #[arg(long, value_enum, required_if_eq("mode", "exact"))]
     pub solver: Option<ExactSolverType>,
 
-    #[arg(short, long, default_value_t = false)]
+    #[arg(long, default_value_t = false)]
     pub show: bool,
 
-    #[arg(short, long, default_value_t = false)]
+    #[arg(long, default_value_t = false)]
     pub save: bool,
 
-    #[arg(short, long, default_value_t = 60)]
+    #[arg(long, default_value_t = 60)]
     pub limit: u64,
 }
 
