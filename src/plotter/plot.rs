9cc7e952a703734532aa5eb34292771459c73597
othiagos <thg.santos@outlook.com>
2026-02-25 15:35:23 -0300

    Execute the script when arguments exist

--- src/plotter/plot.rs
+++ src/plotter/plot.rs
@@ -5,20 +5,17 @@ const PYTHON_SCRIPT: &str = include_str!("../../script/visualizer.py");
 pub fn show(path: &str, show: bool, save: bool) {
     let mut cmd = Command::new("python3");
 
-    cmd
-        .arg("-c")
-        .arg(PYTHON_SCRIPT)
-        .arg(path);
+    cmd.arg("-c").arg(PYTHON_SCRIPT).arg(path);
 
     if show {
         cmd.arg("show");
     }
-    
+ 
     if save {
         cmd.arg("save");
     }
 
-    if let Err(e) = cmd.status() {
+    if (show || save) && let Err(e) = cmd.status() {
         eprintln!("Failed to execute script: {}", e);
     }
 }
